381880f768705c8e2a76c63f4212d6166a636dad
Joseph Lamb <jlambpco@gmail.com>
2022-11-23 14:02:27 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express')
+const { authMiddleware } = require('./utils/auth')
 
 const { typeDefs, resolvers } = require('./schemas')
 
@@ -11,7 +12,8 @@ const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
 app.use(express.urlencoded({ extended: false }));
