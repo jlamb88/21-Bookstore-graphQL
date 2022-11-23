d665b178d3c01a6f3ebc1f34d4dfb09794ccfe45
Joseph Lamb <jlambpco@gmail.com>
2022-11-23 10:15:34 -0500

    heroku commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jlamb-admin:scotland1md@cluster0.dcrn6ph.mongodb.net/?retryWrites=true&w=majority')//, { || 'mongodb://localhost/googlebooks', {
-// useNewUrlParser: true,
-// useUnifiedTopology: true,
+mongoose.connect('mongodb+srv://jlamb-admin:scotland1md@cluster0.dcrn6ph.mongodb.net/mod18?retryWrites=true&w=majority'),//, { || 'mongodb://localhost/googlebooks', {
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
 // useCreateIndex: true,
 // useFindAndModify: false,
 // });
