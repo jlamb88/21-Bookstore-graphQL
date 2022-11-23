bc165005cde4c71b8d2d092f820787ff606a618d
Joseph Lamb <jlambpco@gmail.com>
2022-11-23 14:33:10 -0500

    moved params into connect call

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jlamb-admin:scotland1md@cluster0.dcrn6ph.mongodb.net/mod21?retryWrites=true&w=majority'),//, { || 'mongodb://localhost/googlebooks', {
-{
+mongoose.connect('mongodb+srv://jlamb-admin:scotland1md@cluster0.dcrn6ph.mongodb.net/mod21?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}
+});//, { || 'mongodb://localhost/googlebooks', {
+
 // useCreateIndex: true,
 // useFindAndModify: false,
 // });
